5cef3119f38c6c5f9fab3c1f4429c63fa0d1aab4
Khalil <khalil.bm@hotmail.com>
2023-09-20 01:48:22 -0300

    implementando funcionalidades editar e excluir

--- src/components/EditTodoForm/index.js
+++ src/components/EditTodoForm/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const EditTodoForm = () => {
-  return (
-    <div>EditTodoForm</div>
-  )
+
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState(task.task)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        editTodo(value, task.id);
+
+        setValue("");
+    }
+    return (
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type='text' className='todo-input' value={value} placeholder='Digite o item a ser editado' 
+            onChange={(event) => setValue(event.target.value)}/>
+                <button type='submit' className='todo-btn'>Atualizar item</button>
+        </form>
+    )
 }
